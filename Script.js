d7f65d23543ea32b70991fcac90785eb9ee4111f
poetarung-maker <poetarung@gmail.com>
2026-01-29 10:32:19 +0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
+// Contoh sederhana: efek scroll halus
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
 
+// Bisa ditambah animasi fade-in ketika scroll, dll
+console.log("Website Proklamasi siap!");
